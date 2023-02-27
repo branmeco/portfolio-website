3e5799eff3e73db9df3c4193b003f7b78ce581e0
branmeco <branmeco17@gmail.com>
2023-02-27 16:16:17 -0500

    Services Components

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from './Components/header/Header';
 import Home from './Components/home/Home';
 import About from './Components/about/About';
 import Skills from './Components/skills/Skills';
+import Services from './Components/services/Services';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Home />
         <About />
         <Skills/>
+        <Services/>
       </main>
     </>
   )
